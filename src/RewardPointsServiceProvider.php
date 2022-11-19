5a3349884db6f23d38d83ccc8a9d42bed6cdd7f9
Karim Mohamed <komicho1996@gmail.com>
2022-11-19 11:21:27 +0200

    Feat: create `ServiceProvider` for this package

---
+++ src/RewardPointsServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace OutMart\Laravel\RewardPoints;
+
+use Illuminate\Support\ServiceProvider;
+
+class RewardPointsServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
