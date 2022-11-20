ef32e2c4ddd3f0f9b0ff9868b98673b1bf111190
Moamen Eltouny <raggigroup@gmail.com>
2022-11-20 18:20:57 +0200

    add docs to the model, add datetime casts too

--- src/Models/RewardPoint.php
+++ src/Models/RewardPoint.php
@@ -4,6 +4,17 @@ namespace OutMart\Laravel\RewardPoints\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Reward Points Model of OutMart
+ * 
+ * @property integer $id
+ * @property string $type
+ * @property integer $points
+ * @property string|null $comment
+ * @property \Carbon\Carbon $expired_at
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ */
 class RewardPoint extends Model
 {
     /**
@@ -23,5 +34,14 @@ class RewardPoint extends Model
         'points',
         'comment',
         'expired_at',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'expired_at' => 'datetime'
     ];
 }
